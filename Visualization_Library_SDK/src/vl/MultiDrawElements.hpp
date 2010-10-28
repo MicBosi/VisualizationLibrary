e84bdca2cc384c934f637da71d0d9808c8eb7ac8
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2010-10-28 00:15:22 +0000

    minor documentation update

--- Visualization_Library_SDK/src/vl/MultiDrawElements.hpp
+++ Visualization_Library_SDK/src/vl/MultiDrawElements.hpp
@@ -52,11 +52,11 @@ namespace vl
   {
   public:
     /** Returns the special index which idendifies a primitive restart. By default it is set to ~0 that is 
-      * 0xFF, 0xFFFF, 0xFFFFFFFF respectively for ubyte, ushort, uint types. */
+      * 0xFF, 0xFFFF, 0xFFFFFFFF respectively for ubyte, ushort, uint index types. */
     GLuint primitiveRestartIndex() const { return mPrimitiveRestartIndex; }
 
     /** Sets the special index which idendifies a primitive restart. By default it is set to ~0 that is 
-      * 0xFF, 0xFFFF, 0xFFFFFFFF respectively for ubyte, ushort, uint types. */
+      * 0xFF, 0xFFFF, 0xFFFFFFFF respectively for ubyte, ushort, uint index types. */
     void setPrimitiveRestartIndex(GLuint index) { mPrimitiveRestartIndex = index; }
 
     /** Returns whether the primitive-restart functionality is enabled or not. See http://www.opengl.org/registry/specs/NV/primitive_restart.txt */
