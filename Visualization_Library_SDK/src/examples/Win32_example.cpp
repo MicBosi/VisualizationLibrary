7b85258a4c89fb010ccf8eb95a9608dc6917645e
Michele <hello@michelebosi.com>
2010-10-28 08:44:46 +0000

    snapshot version 2010.10.1111

--- Visualization_Library_SDK/src/examples/Win32_example.cpp
+++ Visualization_Library_SDK/src/examples/Win32_example.cpp
@@ -59,6 +59,17 @@ int APIENTRY WinMain(HINSTANCE /*hCurrentInst*/, HINSTANCE /*hPreviousInst*/, LP
   applet->initialize();
   /* create a native Win32 window */
   vl::ref<vlWin32::Win32Window> win32_window = new vlWin32::Win32Window;
+
+  int attribs[] =
+  {
+    WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+    WGL_CONTEXT_MINOR_VERSION_ARB, 3, 
+    WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
+    WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+    0
+  };
+  win32_window->setContextAttribs(attribs);
+
   /* bind the applet so it receives all the GUI events related to the OpenGLContext */
   win32_window->addEventListener(applet.get());
   /* target the window so we can render on it */
