1c4cac897b94805920611bf8d186778c99fef7a4
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2010-12-05 13:31:45 +0000

    Mac OS X support fixes.

--- trunk/Visualization_Library_SDK/src/examples/Cocoa_tests.mm
+++ trunk/Visualization_Library_SDK/src/examples/Cocoa_tests.mm
@@ -3,7 +3,7 @@
 /*  Visualization Library                                                             */
 /*  http://www.visualizationlibrary.com                                               */
 /*                                                                                    */
-/*  Copyright (c) 2005-2010, Michele Bosi                                             */
+/*  Copyright (c) 2005-2010, Michele Bosi, Brent Fulgham                              */
 /*  All rights reserved.                                                              */
 /*                                                                                    */
 /*  Redistribution and use in source and binary forms, with or without modification,  */
