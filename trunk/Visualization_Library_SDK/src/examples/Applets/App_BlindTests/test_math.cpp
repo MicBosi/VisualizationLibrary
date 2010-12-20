ec420de153d90116c92ebd6c8b2220f864d53d35
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2010-12-20 01:47:36 +0000

    various Quaternion fixes and regression tests (to be continued...)

--- trunk/Visualization_Library_SDK/src/examples/Applets/App_BlindTests/test_math.cpp
+++ trunk/Visualization_Library_SDK/src/examples/Applets/App_BlindTests/test_math.cpp
@@ -30,9 +30,19 @@
 /**************************************************************************************/
 
 #include "vl/Quaternion.hpp"
+#include "vl/Say.hpp"
+#include "vl/Log.hpp"
 #include <set>
 
 using namespace vl;
+
+#define CONDITION(cond) \
+  if (!(cond)) \
+  { \
+    vl::Log::print( Say("%s %n: condition \""#cond"\" failed.\n") << __FILE__ << __LINE__); \
+    VL_CHECK(0); \
+    return false; \
+  }
 
 namespace blind_tests
 {
@@ -42,7 +52,6 @@ namespace blind_tests
     // 2) implementa operator+() basato su operator+=() e non viceversa -> quaternion e anche tutte le altre classi
 
     // quaternion compilation and functionality tests
-
 
     std::set<fquat> qset;
     qset.insert(fquat());
@@ -55,21 +64,77 @@ namespace blind_tests
     q1 = q2;
     q1 = (fquat)qd;
     v1 = q1 * v1;
-    bool ok = q1 == q2;
-    ok = q1 != q2;
+
+    q1 = fquat(4,3,2,1);
+    q2 = fquat(4,3,2,1);
+    CONDITION(q1 == q2 && !(q1 != q2))
+    q2 = fquat(3,2,1,0);
+    CONDITION(q1 != q2 && !(q1 == q2))
+    
+    q1 = fquat(1,2,3,4);
     q1.setZero();
+    CONDITION( q1 == fquat(0,0,0,0) );
+    
+    q1 = fquat(1,2,3,4);
     q1.setNoRotation();
-    q1.fromVectors(fvec4(1,2,3,4), fvec4(1,2,3,4));
-    q1.fromMatrix( fmat4::rotation(45, 0, 1, 0) );
+    CONDITION( q1 == fquat(0,0,0,1) );
+
+    fvec3 v3; float angle = 0;
+    q1.fromAxisAngle(fvec3(0,1,0), 90);
+    q1.toAxisAngle(v3, angle);
+    CONDITION( distance(v3, fvec3(0,1,0)) < 0.001f );
+    CONDITION( angle == 90 );
+
+    q1.fromVectors(fvec3(1,0,0), fvec3(-1,0,-1));
+    q1.toAxisAngle(v3, angle);
+    CONDITION( distance(v3, fvec3(0,1,0)) < 0.001f );
+    CONDITION( angle == 135 );
+
+    q1.fromMatrix( fmat4::rotation(0, 1, 1, 0) );
+    q1.toAxisAngle(v3, angle);
+    CONDITION( v3 == fvec3(0,0,0) )
+    CONDITION( angle == 0 );
+
+    for (float i=5.0; i<180.0; i+=5.0f)
+    {
+      q1.fromMatrix( fmat4::rotation(i, 1, 1, 0) );
+      q1.toAxisAngle(v3, angle);
+      CONDITION( distance(v3, fvec3(0.70710683f,0.70710683f,0)) < 0.001f );
+      CONDITION( fabs(angle - i) < 0.001f );
+
+      q1.fromAxisAngle( fvec3(1,1,0), i );
+      q1.toAxisAngle(v3, angle);
+      CONDITION( distance(v3, fvec3(0.70710683f,0.70710683f,0)) < 0.001f );
+      CONDITION( fabs(angle - i) < 0.001f );
+
+      q1.fromMatrix( fmat4::rotation(-i, 1, 1, 0) );
+      q1.toAxisAngle(v3, angle);
+      CONDITION( distance(v3, fvec3(-0.70710683f,-0.70710683f,0)) < 0.001f );
+      CONDITION( fabs(angle - i) < 0.001f );
+
+      q1.fromAxisAngle( fvec3(1,1,0), -i );
+      q1.toAxisAngle(v3, angle);
+      CONDITION( distance(v3, fvec3(-0.70710683f,-0.70710683f,0)) < 0.001f );
+      CONDITION( fabs(angle - i) < 0.001f );
+
+      q1.fromMatrix( fmat4::rotation(i, -1, -1, 0) );
+      q1.toAxisAngle(v3, angle);
+      CONDITION( distance(v3, fvec3(-0.70710683f,-0.70710683f,0)) < 0.001f );
+      CONDITION( fabs(angle - i) < 0.001f );
+
+      q1.fromAxisAngle( fvec3(-1,-1,0), i );
+      q1.toAxisAngle(v3, angle);
+      CONDITION( distance(v3, fvec3(-0.70710683f,-0.70710683f,0)) < 0.001f );
+      CONDITION( fabs(angle - i) < 0.001f );
+    }
+
     q1.fromEulerXYZ(10,20,30);
     q1.fromEulerZYX(10,20,30);
-    q1.fromAxisAngle(90, fvec3(1,2,3));
-    fvec3 v3; float angle = 0;
-    q1.toAxisAngle(v3, angle);
+    q1.fromAxisAngle(fvec3(1,2,3), 90);
     fmat4 m1 = q1.toMatrix();
     v1 = q1.xyzw();
     q1 = v1;
 
-    return false;
+    return true;
   }
 }
