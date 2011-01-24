f054a0ad58be938c073fe270bd3c4b84f12bc393
Michele <hello@michelebosi.com>
2011-01-24 01:30:46 +0000

    New image gallery

--- Visualization_Library_SDK/docs/make-HTML.bat
+++ Visualization_Library_SDK/docs/make-HTML.bat
@@ -1,1 +1,9 @@
+mkdir Visualization_Library_Docs
+mkdir Visualization_Library_Docs\gallery
+mkdir Visualization_Library_Docs\gallery_small
+mkdir Visualization_Library_Docs\highslide
+
+xcopy highslide\* Visualization_Library_Docs\highslide /Y /S
+xcopy gallery\* Visualization_Library_Docs\gallery /Y /S
+xcopy gallery_small\* Visualization_Library_Docs\gallery_small /Y /S
 doxygen.exe
