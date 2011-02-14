34fd669a095a44d2c02835724af845e9d97313b3
Michele <hello@michelebosi.com>
2011-02-14 01:02:39 +0000

    polygon simplification demo retouched

--- Visualization_Library_SDK/src/vlGraphics/PolygonSimplifier.cpp
+++ Visualization_Library_SDK/src/vlGraphics/PolygonSimplifier.cpp
@@ -164,17 +164,17 @@ void PolygonSimplifier::simplify(int target_vertex_count, Geometry* geom)
 //-----------------------------------------------------------------------------
 void PolygonSimplifier::simplify(int target_vertex_count, std::vector<fvec3>& in_out_verts, std::vector<int>& in_out_tris)
 {
-  mSimplifiedVertices.clear();
-  mSimplifiedTriangles.clear();
-  mProtectedVerts.clear();
-  mTriangleLump.clear();
-  mVertexLump.clear();
-
   if (verbose())
     Log::print("PolygonSimplifier::simplify() starting ... \n");
 
   Time timer;
   timer.start();
+
+  mSimplifiedVertices.clear();
+  mSimplifiedTriangles.clear();
+  mProtectedVerts.clear();
+  mTriangleLump.clear();
+  mVertexLump.clear(); // mic fixme: this is the one taking time to clearup.
 
   // preallocate vertices and triangles in one chunk
   mTriangleLump.resize(in_out_tris.size()/3);
