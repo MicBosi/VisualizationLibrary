84996427c122318afad8e53b63bfa3df52edacc1
Michele <hello@michelebosi.com>
2011-02-14 09:05:29 +0000

    Sphere class docs update

--- Visualization_Library_SDK/src/vlCore/Sphere.hpp
+++ Visualization_Library_SDK/src/vlCore/Sphere.hpp
@@ -39,30 +39,41 @@ namespace vl
 //-----------------------------------------------------------------------------
 // Sphere
 //-----------------------------------------------------------------------------
-  /**
-   * The Sphere class defines a sphere using a center and a radius using vl::Real precision.
-  */
+  /** The Sphere class defines a sphere using a center and a radius using vl::Real precision. */
   class Sphere
   {
   public:
-    //! Constructor.
+    /** Constructor: creates a null sphere. */
     Sphere(): mRadius(-1) { }
 
-    //! Constructor.
+    /** Constructor: creates a sphere with the given center and radius. */
     Sphere(const vec3& center, Real radius): mCenter(center), mRadius(radius) {}
 
-    //! Constructor.
+    /** Copy-constructor. */
     Sphere(const AABB& aabb) { *this = aabb; }
 
+    /** Sets the sphere as null.*/
     void setNull()  { mRadius =-1.0f; mCenter = vec3(0,0,0); }
-    void setPoint() { mRadius = 0.0f; /*mCenter = vec3(0,0,0);*/ }
+
+    /** Returns true if the sphere is null, ie, if radius is < 0. */    
     bool isNull()  const { return mRadius <  0.0f; }
+    
+    /** Returns true if the sphere as radius == 0 */
     bool isPoint() const { return mRadius == 0.0f; }
+    
+    /** Sets the center of the sphere. */
     void setCenter(const vec3& center) { mCenter = center; }
+    
+    /** Returns the center of the sphere. */
     const vec3& center() const { return mCenter; }
+    
+    /** Sets the radius of the sphere. */
     void setRadius( Real radius ) { mRadius = radius; }
+    
+    /** Returns the radius of the sphere. */
     Real radius() const { return mRadius; }
 
+    /** Returns true if a sphere contains the specified sphere. */
     bool includes(const Sphere& other) const
     {
       if (isNull())
@@ -77,36 +88,39 @@ namespace vl
       }
     }
 
+    /** Returns true if two spheres are identical. */
     bool operator==(const Sphere& other) const 
     {
       return mCenter == other.mCenter && mRadius == other.mRadius;
     }
 
+    /** Returns true if two spheres are not identical. */
     bool operator!=(const Sphere& other) const
     {
       return !operator==(other);
     }
     
+    /** Constructs a sphere that contains the specified AABB. */
     Sphere& operator=(const AABB& aabb)
     {
       if (aabb.isNull())
         setNull();
       else
       {
-        // center the sphere on the aabb center
         mCenter = aabb.center();
-        // half of the maximum diagonal
         mRadius = (aabb.minCorner() - aabb.maxCorner()).length() / (Real)2.0;
       }
       return *this;
     }
 
+    /** Returns a sphere that contains the two specified spheres. */
     Sphere operator+(const Sphere& other)
     {
       Sphere t = *this;
       return t += other;
     }
 
+    /** Enlarges the sphere to contain the specified sphere. */
     const Sphere& operator+=(const Sphere& other)
     {
       if (this->isNull())
@@ -117,7 +131,7 @@ namespace vl
         *this = other;
       }
       else
-      if (!other.isNull() && !this->includes(other) )
+      if (!other.isNull() && !this->includes(other))
       {
         vec3 v = other.center() - this->center();
         if (v.isNull())
@@ -139,30 +153,31 @@ namespace vl
       return *this;
     }
 
-    void transformed(Sphere& sphere, const mat4& mat) const 
+    /** Returns a sphere that contains the original sphere transformed by the given matrix. */
+    void transformed(Sphere& out, const mat4& mat) const 
     {
-      sphere.setNull();
+      out.setNull();
       if ( !isNull() )
       {
-        sphere.mCenter = mat * center();
+        out.mCenter = mat * center();
         // vec3 p = center() + vec3( (Real)0.577350269189625840, (Real)0.577350269189625840, (Real)0.577350269189625840 ) * radius();
         // p = mat * p;
-        // p = p - sphere.center();
-        // sphere.setRadius(p.length());
+        // p = p - out.center();
+        // out.setRadius(p.length());
         vec3 p0 = center() + vec3(radius(),0,0);
         vec3 p1 = center() + vec3(0,radius(),0);
         vec3 p2 = center() + vec3(0,0,radius());
         p0 = mat * p0;
         p1 = mat * p1;
         p2 = mat * p2;
-        Real d0 = (p0 - sphere.mCenter).lengthSquared();
-        Real d1 = (p1 - sphere.mCenter).lengthSquared();
-        Real d2 = (p2 - sphere.mCenter).lengthSquared();
-        sphere.mRadius = d0>d1 ? (d0>d2?d0:d2) : (d1>d2?d1:d2);
-        sphere.mRadius = ::sqrt(sphere.mRadius);
+        Real d0 = (p0 - out.mCenter).lengthSquared();
+        Real d1 = (p1 - out.mCenter).lengthSquared();
+        Real d2 = (p2 - out.mCenter).lengthSquared();
+        out.mRadius = ::sqrt( d0>d1 ? (d0>d2?d0:d2) : (d1>d2?d1:d2) );
       }
     }
 
+    /** Returns a sphere that contains the original sphere transformed by the given matrix. */
     Sphere transformed(const mat4& mat) const 
     {
       Sphere sphere;
