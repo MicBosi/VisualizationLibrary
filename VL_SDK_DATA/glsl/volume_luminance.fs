a3657c1b8549c6a71beb354b23108250bd56a5ea
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2011-02-24 23:02:54 +0000

    added copyright notice

--- VL_SDK_DATA/glsl/volume_luminance.fs
+++ VL_SDK_DATA/glsl/volume_luminance.fs
@@ -1,3 +1,16 @@
+/**************************************************************************************/
+/*                                                                                    */
+/*  Copyright (c) 2005-2010, Michele Bosi.                                            */
+/*  All rights reserved.                                                              */
+/*                                                                                    */
+/*  This file is part of Visualization Library                                        */
+/*  http://www.visualizationlibrary.com                                               */
+/*                                                                                    */
+/*  Released under the OSI approved Simplified BSD License                            */
+/*  http://www.opensource.org/licenses/bsd-license.php                                */
+/*                                                                                    */
+/**************************************************************************************/
+
 // This shader directly maps the volume value to the transfer function with no
 // lighting. This shader is to be used with IF_LUMINANCE image volumes.
 
