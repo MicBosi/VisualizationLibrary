033793e06e650827596cdc2db497ad966ecfc742
Michele <hello@michelebosi.com>
2011-04-07 08:39:06 +0000

    coherent tags names

---
+++ VL_SDK_SRC/src/vlCore/init_core.cpp
@@ -0,0 +1,289 @@
+/**************************************************************************************/
+/*                                                                                    */
+/*  Visualization Library                                                             */
+/*  http://www.visualizationlibrary.com                                               */
+/*                                                                                    */
+/*  Copyright (c) 2005-2010, Michele Bosi                                             */
+/*  All rights reserved.                                                              */
+/*                                                                                    */
+/*  Redistribution and use in source and binary forms, with or without modification,  */
+/*  are permitted provided that the following conditions are met:                     */
+/*                                                                                    */
+/*  - Redistributions of source code must retain the above copyright notice, this     */
+/*  list of conditions and the following disclaimer.                                  */
+/*                                                                                    */
+/*  - Redistributions in binary form must reproduce the above copyright notice, this  */
+/*  list of conditions and the following disclaimer in the documentation and/or       */
+/*  other materials provided with the distribution.                                   */
+/*                                                                                    */
+/*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND   */
+/*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED     */
+/*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE            */
+/*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR  */
+/*  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    */
+/*  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;      */
+/*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON    */
+/*  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           */
+/*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS     */
+/*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                      */
+/*                                                                                    */
+/**************************************************************************************/
+
+#include <vlCore/VisualizationLibrary.hpp>
+#include <vlCore/VLSettings.hpp>
+#include <vlCore/FileSystem.hpp>
+#include <vlCore/LoadWriterManager.hpp>
+#include <vlCore/Log.hpp>
+#include <vlCore/Say.hpp>
+#include <vlCore/Quaternion.hpp>
+#include <vlCore/AABB.hpp>
+#include <vlCore/Sphere.hpp>
+#include <vlCore/version.hpp>
+#include <cassert>
+
+using namespace vl;
+
+#if defined(VL_IO_2D_JPG)
+  #include <vlCore/vlJPG.hpp>
+#endif
+#if defined(VL_IO_2D_PNG)
+  #include <vlCore/vlPNG.hpp>
+#endif
+#if defined(VL_IO_2D_TIFF)
+  #include <vlCore/vlTIFF.hpp>
+#endif
+#if defined(VL_IO_2D_TGA)
+  #include <vlCore/vlTGA.hpp>
+#endif
+#if defined(VL_IO_2D_DAT)
+  #include <vlCore/vlDAT.hpp>
+#endif
+#if defined(VL_IO_2D_DDS)
+  #include <vlCore/vlDDS.hpp>
+#endif
+#if defined(VL_IO_2D_BMP)
+  #include <vlCore/vlBMP.hpp>
+#endif
+#if defined(VL_IO_2D_DICOM)
+  #include <vlCore/vlDICOM.hpp>
+#endif
+
+//------------------------------------------------------------------------------
+VL_COMPILE_TIME_CHECK( sizeof(double)    == 8 )
+VL_COMPILE_TIME_CHECK( sizeof(float)     == 4 )
+VL_COMPILE_TIME_CHECK( sizeof(long long) == 8 )
+VL_COMPILE_TIME_CHECK( sizeof(int)       == 4 )
+VL_COMPILE_TIME_CHECK( sizeof(short)     == 2 )
+VL_COMPILE_TIME_CHECK( sizeof(char)      == 1 )
+VL_COMPILE_TIME_CHECK( sizeof(wchar_t)   >= 2 )
+VL_COMPILE_TIME_CHECK( sizeof(vec2)      == sizeof(Real)*2 )
+VL_COMPILE_TIME_CHECK( sizeof(vec3)      == sizeof(Real)*3 )
+VL_COMPILE_TIME_CHECK( sizeof(vec4)      == sizeof(Real)*4 )
+VL_COMPILE_TIME_CHECK( sizeof(mat2)      == sizeof(Real)*2*2 )
+VL_COMPILE_TIME_CHECK( sizeof(mat3)      == sizeof(Real)*3*3 )
+VL_COMPILE_TIME_CHECK( sizeof(mat4)      == sizeof(Real)*4*4 )
+VL_COMPILE_TIME_CHECK( sizeof(quat)      == sizeof(Real)*4 )
+VL_COMPILE_TIME_CHECK( sizeof(AABB)      == sizeof(Real)*6 )
+VL_COMPILE_TIME_CHECK( sizeof(Sphere)    == sizeof(Real)*4 )
+//------------------------------------------------------------------------------
+// VL misc
+//------------------------------------------------------------------------------
+namespace
+{
+  std::string gVersionString = String( Say("%n.%n.%n") << VL_Major << VL_Minor << VL_Build ).toStdString();
+  const char* gCertificate = "[Visualization Library BSD License]";
+  bool gInitializedCore = false;
+};
+//------------------------------------------------------------------------------
+bool VisualizationLibrary::isCoreInitialized() { return gInitializedCore; }
+//------------------------------------------------------------------------------
+const char* VisualizationLibrary::versionString() { return gVersionString.c_str(); }
+//------------------------------------------------------------------------------
+// Global VLSettings
+//------------------------------------------------------------------------------
+namespace
+{
+  ref<VLSettings> gSettings = NULL;
+}
+VLSettings* vl::globalSettings()
+{
+  return gSettings.get();
+}
+//------------------------------------------------------------------------------
+// Default logger
+//------------------------------------------------------------------------------
+namespace
+{
+  ref<Log> gDefaultLogger;
+}
+void vl::setDefLogger(Log* logger) 
+{ 
+  gDefaultLogger = logger; 
+}
+Log* vl::defLogger() 
+{ 
+  return gDefaultLogger.get(); 
+}
+//------------------------------------------------------------------------------
+// Default LoadWriterManager
+//------------------------------------------------------------------------------
+namespace 
+{
+  ref<LoadWriterManager> gDefaultLoadWriterManager = NULL;
+}
+LoadWriterManager* vl::defLoadWriterManager()
+{
+  return gDefaultLoadWriterManager.get();
+}
+void vl::setDefLoadWriterManager(LoadWriterManager* lwm)
+{
+  gDefaultLoadWriterManager = lwm;
+}
+//-----------------------------------------------------------------------------
+// Default FileSystem
+//-----------------------------------------------------------------------------
+namespace 
+{
+  ref<FileSystem> gDefaultFileSystem = NULL;;
+}
+FileSystem* vl::defFileSystem()
+{
+  return gDefaultFileSystem.get();
+}
+void vl::seDefFileSystem(FileSystem* fs)
+{
+  gDefaultFileSystem = fs;
+}
+//------------------------------------------------------------------------------
+void VisualizationLibrary::initCore()
+{
+  VL_CHECK(!gInitializedCore);
+  if (gInitializedCore)
+  {
+    Log::bug("VisualizationLibrary::initCore(): Visualization Library Core is already initialized!\n");
+    return;
+  }
+
+  // --- Init Core ---
+
+  // Install globabl settings
+  gSettings = new VLSettings;
+
+  // Install default logger
+  ref<StandardLog> logger = new StandardLog;
+  logger->setLogFile( globalSettings()->defaultLogPath() );
+  setDefLogger( logger.get() );
+
+  // Install default LoadWriterManager
+  gDefaultLoadWriterManager = new LoadWriterManager;
+
+  // Install default FileSystem
+  gDefaultFileSystem = new FileSystem;
+  gDefaultFileSystem->directories()->push_back( new DiskDirectory( globalSettings()->defaultDataPath() ) );
+  
+  // Register 2D modules
+  #if defined(VL_IO_2D_JPG)
+    registerLoadWriter(new LoadWriterJPG);
+  #endif
+  #if defined(VL_IO_2D_PNG)
+    registerLoadWriter(new LoadWriterPNG);
+  #endif
+  #if defined(VL_IO_2D_TIFF)
+    registerLoadWriter(new LoadWriterTIFF);
+  #endif
+  #if defined(VL_IO_2D_TGA)
+    registerLoadWriter(new LoadWriterTGA);
+  #endif
+  #if defined(VL_IO_2D_BMP)
+    registerLoadWriter(new LoadWriterBMP);
+  #endif
+  #if defined(VL_IO_2D_DDS)
+    registerLoadWriter(new LoadWriterDDS);
+  #endif
+  #if defined(VL_IO_2D_DAT)
+    registerLoadWriter(new LoadWriterDAT);
+  #endif
+  #if defined(VL_IO_2D_DICOM)
+    registerLoadWriter(new LoadWriterDICOM);
+  #endif
+
+  // Log VL and system information.
+  if (globalSettings()->verbosityLevel())
+    Log::logSystemInfo();
+
+  // Initialized = on
+  gInitializedCore = true;
+}
+//------------------------------------------------------------------------------
+void VisualizationLibrary::shutdownCore()
+{
+  // Initialized = off
+  gInitializedCore = false;
+
+  // --- Dispose Core ---
+
+  // Dispose default LoadWriterManager
+  gDefaultLoadWriterManager->loadCallbacks()->clear();
+  gDefaultLoadWriterManager->writeCallbacks()->clear();
+  gDefaultLoadWriterManager->loadWriters()->clear();
+  gDefaultLoadWriterManager = NULL;
+
+  // Dispose default FileSystem
+  gDefaultFileSystem->directories()->clear();
+  gDefaultFileSystem = NULL;
+
+  // Dispose default logger
+  if (globalSettings()->verbosityLevel())
+  {
+    Log::print("Visualization Library shutdown.\n");
+  }
+  setDefLogger( NULL );
+
+  // Dispose globabl settings
+  gSettings = NULL;
+}
+//------------------------------------------------------------------------------
+#if !defined(VL_MODULE_GRAPHICS)
+  void VisualizationLibrary::init()
+  {
+    initCore();
+  }
+//------------------------------------------------------------------------------
+  void VisualizationLibrary::shutdown()
+  {
+    if (gInitializedCore)
+    {
+      shutdownCore();
+    }
+  }
+//------------------------------------------------------------------------------
+  bool VisualizationLibrary::isGraphicsInitialized() { return false; }
+#endif
+//------------------------------------------------------------------------------
+#if defined(_WIN32)
+  // console includes
+  #include <stdio.h>
+  #include <io.h>
+  #include <fcntl.h>
+  #include <commdlg.h>
+#endif
+
+void vl::showWin32Console()
+{
+  #if defined(_WIN32)
+    AllocConsole();
+    // stdout
+    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    int hCrt = _open_osfhandle((intptr_t)handle_out, _O_TEXT);
+    FILE* hf_out = _fdopen(hCrt, "w"); 
+    setvbuf(hf_out, NULL, _IONBF, 1);
+    *stdout = *hf_out;
+    // stderr
+    handle_out = GetStdHandle(STD_ERROR_HANDLE);
+    hCrt = _open_osfhandle((intptr_t)handle_out, _O_TEXT);
+    hf_out = _fdopen(hCrt, "w");
+    setvbuf(hf_out, NULL, _IONBF, 1);
+    *stderr = *hf_out;
+  #endif
+}
+//------------------------------------------------------------------------------
---
+++ VL_SDK_SRC/src/vlCore/vlTGA.hpp
@@ -0,0 +1,99 @@
+/**************************************************************************************/
+/*                                                                                    */
+/*  Visualization Library                                                             */
+/*  http://www.visualizationlibrary.com                                               */
+/*                                                                                    */
+/*  Copyright (c) 2005-2010, Michele Bosi                                             */
+/*  All rights reserved.                                                              */
+/*                                                                                    */
+/*  Redistribution and use in source and binary forms, with or without modification,  */
+/*  are permitted provided that the following conditions are met:                     */
+/*                                                                                    */
+/*  - Redistributions of source code must retain the above copyright notice, this     */
+/*  list of conditions and the following disclaimer.                                  */
+/*                                                                                    */
+/*  - Redistributions in binary form must reproduce the above copyright notice, this  */
+/*  list of conditions and the following disclaimer in the documentation and/or       */
+/*  other materials provided with the distribution.                                   */
+/*                                                                                    */
+/*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND   */
+/*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED     */
+/*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE            */
+/*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR  */
+/*  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    */
+/*  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;      */
+/*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON    */
+/*  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           */
+/*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS     */
+/*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                      */
+/*                                                                                    */
+/**************************************************************************************/
+
+#if !defined(ioTGA_INCLUDE_ONCE)
+#define ioTGA_INCLUDE_ONCE
+
+#include <vlCore/ResourceLoadWriter.hpp>
+#include <vlCore/ResourceDatabase.hpp>
+#include <vlCore/Image.hpp>
+
+namespace vl
+{
+  class VirtualFile;
+  class String;
+  class Image;
+
+  ref<Image> loadTGA(VirtualFile* file);
+  ref<Image> loadTGA(const String& path);
+  bool isTGA(VirtualFile* file);
+  bool saveTGA(const Image* src, const String& path);
+  bool saveTGA(const Image* src, VirtualFile* file);
+
+  //---------------------------------------------------------------------------
+  // LoadWriterTGA
+  //---------------------------------------------------------------------------
+  /**
+   * The LoadWriterTGA class is a ResourceLoadWriter capable of reading TGA files.
+   */
+  class LoadWriterTGA: public ResourceLoadWriter
+  {
+  public:
+    virtual const char* className() { return "LoadWriterTGA"; }
+    LoadWriterTGA(): ResourceLoadWriter("|tga|", "|tga|") {}
+
+    ref<ResourceDatabase> loadResource(const String& path) const 
+    {
+      ref<ResourceDatabase> res_db = new ResourceDatabase;
+      ref<Image> img = loadTGA(path);
+      if (img)
+        res_db->resources().push_back(img);
+      return res_db;
+    }
+
+    ref<ResourceDatabase> loadResource(VirtualFile* file) const
+    {
+      ref<ResourceDatabase> res_db = new ResourceDatabase;
+      ref<Image> img = loadTGA(file);
+      if (img)
+        res_db->resources().push_back(img);
+      return res_db;
+    }
+
+    bool writeResource(const String& path, ResourceDatabase* resource) const
+    {
+      bool ok = true;
+      for(unsigned i=0; i<resource->count<Image>(); ++i)
+        ok &= saveTGA(resource->get<Image>(i), path);
+      return ok;
+    }
+
+    bool writeResource(VirtualFile* file, ResourceDatabase* resource) const
+    {
+      bool ok = true;
+      for(unsigned i=0; i<resource->count<Image>(); ++i)
+        ok &= saveTGA(resource->get<Image>(i), file);
+      return ok;
+    }
+  };
+}
+
+#endif
