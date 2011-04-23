ce6d42f29a0a77312b83790b70d1d3a9888e881e
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2011-04-23 23:23:39 +0000

    created tag https://visualizationlibrary.googlecode.com/svn/tags/Visualization_Library_SDK-2011.05.1140RC1

---
+++ tags/Visualization_Library_SDK-2011.05.1140RC1/cmake/FindVL.cmake
@@ -0,0 +1,64 @@
+#	Find the Visualization Library.
+#
+#	Sample usage:
+#		find_package(VL COMPONENTS VLCore VLGraphics VLVolume REQUIRED)
+#		include(${VL_INCLUDE_DIRS})
+#		target_link_libraries(myexe ${VL_LIBRARIES})
+#
+#	The following variables are accepted as input:
+#		VL_ROOT (as a CMake or environment variable)
+#			- Directory where VL was installed (the "install prefix").
+#
+#	The following variables are defined by the module:
+#		VL_FOUND - Whether VL was found.
+#		VL_INCLUDE_DIRS - Directories for including the VL module headers.
+#		VL_LIBRARIES - Path to all the requested VL libraries.
+#
+#	This module also defines variables for each requested VL component:
+#		VL_<component-name>_LIBRARIES - The component's libraries (or empty
+#			if the component is not available).
+#
+
+macro(_vl_find_library _var)
+	find_library( ${_var}
+		NAMES ${ARGN}
+		PATHS ${VL_ROOT} ENV VL_ROOT
+		PATH_SUFFIXES "lib"
+	)
+endmacro()
+
+macro(_vl_find_component _name)
+	_vl_find_library(VL_${_name}_LIBRARY "${_component_name}")
+	_vl_find_library(VL_${_name}_LIBRARY_DEBUG "${_component_name}-d")
+
+	if(VL_${_name}_LIBRARY AND VL_${_name}_LIBRARY_DEBUG)
+		set(VL_${_name}_LIBRARIES optimized ${VL_${_name}_LIBRARY} debug ${VL_${_name}_LIBRARY_DEBUG})
+	elseif(VL_${_name}_LIBRARY)
+		set(VL_${_name}_LIBRARIES optimized ${VL_${_name}_LIBRARY})
+	elseif(VL_${_name}_LIBRARY_DEBUG)
+		set(VL_${_name}_LIBRARIES debug ${VL_${_name}_LIBRARY_DEBUG})
+	else()
+		set(VL_${_name}_LIBRARIES "")
+	endif()
+
+	mark_as_advanced(VL_${_name}_LIBRARY VL_${_name}_LIBRARY_DEBUG)
+
+	if(VL_${_name}_LIBRARIES)
+		list(APPEND VL_LIBRARIES ${VL_${_name}_LIBRARIES})
+	endif()
+endmacro()
+
+set(_vl_required_vars VL_INCLUDE_DIRS)
+find_path(VL_INCLUDE_DIRS "vlCore/VisualizationLibrary.hpp"
+	PATHS ${VL_ROOT} ENV VL_ROOT
+	PATH_SUFFIXES "include"
+)
+
+# Find the requested VL components
+foreach(component_name ${VL_FIND_COMPONENTS})
+	_vl_find_component(${component_name})
+	list(APPEND _vl_required_vars VL_${name}_LIBRARIES)
+endforeach(component_name)
+
+include( FindPackageHandleStandardArgs )
+find_package_handle_standard_args(VL DEFAULT_MSG ${_vl_required_vars})
---
+++ tags/Visualization_Library_SDK-2011.05.1140RC1/data/glsl/texture_1d_array.fs
@@ -0,0 +1,19 @@
+/**************************************************************************************/
+/*                                                                                    */
+/*  Copyright (c) 2005-2011, Michele Bosi.                                            */
+/*  All rights reserved.                                                              */
+/*                                                                                    */
+/*  This file is part of Visualization Library                                        */
+/*  http://www.visualizationlibrary.com                                               */
+/*                                                                                    */
+/*  Released under the OSI approved Simplified BSD License                            */
+/*  http://www.opensource.org/licenses/bsd-license.php                                */
+/*                                                                                    */
+/**************************************************************************************/
+
+#extension GL_EXT_texture_array: enable
+uniform sampler1DArray sampler0;
+void main(void)
+{
+	gl_FragColor = texture1DArray(sampler0, gl_TexCoord[0].xy );
+}
