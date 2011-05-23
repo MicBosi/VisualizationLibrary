cef7b4f87c23dc10f0cb76d5f8def8949b5b1f84
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2011-05-23 23:35:58 +0000

    Fixed warning in App_Framebuffer_Object

--- data/glsl/tex_multisample.fs
+++ data/glsl/tex_multisample.fs
@@ -20,7 +20,7 @@ void main(void)
 	const int samples = 4;
 
 	ivec2 texcoord = ivec2( vec2(textureSize(ms_texture)) * gl_TexCoord[0].st);
-	vec4 tex_color;
+	vec4 tex_color = vec4(0.0, 0.0, 0.0, 0.0);
 	for(int i=0; i<samples; ++i)
 		tex_color += texelFetch(ms_texture, texcoord, i);
 	tex_color /= samples;
