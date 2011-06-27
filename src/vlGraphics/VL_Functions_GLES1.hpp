b281c5f9845e58b1abadf1b1b2f89f26eadaaac5
Michele <hello@michelebosi.com>
2011-06-27 16:16:13 +0000

    Added VL license notice.

--- src/vlGraphics/VL_Functions_GLES1.hpp
+++ src/vlGraphics/VL_Functions_GLES1.hpp
@@ -1,9 +1,33 @@
-// mic fixme
-//... wrap with VL_... all the functions that are problematic because have OES suffix or because they don`t exist ...
-//... tutto deve essere sempre compilabile ...
-//... metti copyright notice sui nuovi files ...
-//... elimina files che non servono piu ...
-//... aggiungi nuovi file che servono ...
+/**************************************************************************************/
+/*                                                                                    */
+/*  Visualization Library                                                             */
+/*  http://www.visualizationlibrary.com                                               */
+/*                                                                                    */
+/*  Copyright (c) 2005-2010, Michele Bosi                                             */
+/*  All rights reserved.                                                              */
+/*                                                                                    */
+/*  Redistribution and use in source and binary forms, with or without modification,  */
+/*  are permitted provided that the following conditions are met:                     */
+/*                                                                                    */
+/*  - Redistributions of source code must retain the above copyright notice, this     */
+/*  list of conditions and the following disclaimer.                                  */
+/*                                                                                    */
+/*  - Redistributions in binary form must reproduce the above copyright notice, this  */
+/*  list of conditions and the following disclaimer in the documentation and/or       */
+/*  other materials provided with the distribution.                                   */
+/*                                                                                    */
+/*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND   */
+/*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED     */
+/*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE            */
+/*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR  */
+/*  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    */
+/*  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;      */
+/*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON    */
+/*  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           */
+/*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS     */
+/*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                      */
+/*                                                                                    */
+/**************************************************************************************/
 
 #ifndef VL_GL_FUNCTION_WRAPPERS
 #define VL_GL_FUNCTION_WRAPPERS
@@ -1121,14 +1145,8 @@ namespace vl
     VL_UNSUPPORTED_GLES1_FUNC();
   }
 
-
+  //-----------------------------------------------------------------------------
   
-  
-  
-  
-//-----------------------------------------------------------------------------
-  
-  // mic fixme: check this, document this
   inline std::string getOpenGLExtensions()
   {
     VL_CHECK(glGetString(GL_EXTENSIONS));
