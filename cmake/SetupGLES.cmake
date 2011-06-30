38c90144a4a4b6643d3c9cd5fef7363855a9dab2
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2011-06-30 11:41:39 +0000

    Added SetupGLES.cmake

---
+++ cmake/SetupGLES.cmake
@@ -0,0 +1,20 @@
+# mic fixme
+# search IMG and AMD library and include paths automatically
+
+option(VL_GLES1_EXAMPLES "Build GLES 1.x examples" ON)
+
+# set(VL_GLES_INCLUDE_DIR "NOTFOUND" CACHE STRING "Path to GLES include directories")
+set(VL_GLES_INCLUDE_DIR "C:/Imagination Technologies/POWERVR SDK/OGLES/Builds/OGLES/Include" CACHE STRING "Path to GLES include directories")
+
+# set(VL_GLES_LIB_DIR "NOTFOUND" CACHE STRING "Path to directory containing GLES and EGL libraries")
+set(VL_GLES_LIB_DIR "C:/Imagination Technologies/POWERVR SDK/OGLES/Builds/OGLES/WindowsX86/Lib" CACHE STRING "Path to directory containing GLES and EGL libraries")
+
+set(VL_GLES_LIBRARIES "libEGL.lib;libgles_cm.lib" CACHE STRING "GLES and EGL library names")
+
+if (VL_GLES_INCLUDE_DIR)
+	include_directories(${VL_GLES_INCLUDE_DIR})
+endif()
+
+if (VL_GLES_LIB_DIR)
+	link_directories(${VL_GLES_LIB_DIR})
+endif()
