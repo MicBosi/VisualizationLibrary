5d1b32266efd92dcafe6c53e5aeffc0067b02ff1
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2011-07-10 23:24:44 +0000

    local doxygen build bat file

--- trunk/docs/make-HTML.bat
+++ trunk/docs/make-HTML.bat
@@ -1,1 +1,8 @@
-doxygen.exe
+type Doxyfile > Doxyfile.tmp
+echo. >> Doxyfile.tmp
+echo #---------------------- >> Doxyfile.tmp
+echo. >> Doxyfile.tmp
+echo PROJECT_NUMBER=tmp.tmp.tmp >> Doxyfile.tmp
+echo OUTPUT_DIRECTORY=D:/VL/Install/docs >> Doxyfile.tmp
+echo HTML_OUTPUT=html >> Doxyfile.tmp
+doxygen.exe Doxyfile.tmp
