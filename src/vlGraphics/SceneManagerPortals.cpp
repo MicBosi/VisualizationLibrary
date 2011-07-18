dd069ddfdc9938cf73222fb581cc82d4fe1a5f3e
Michele <hello@michelebosi.com>
2011-07-18 11:25:36 +0000

    Removed outdated comments.

--- src/vlGraphics/SceneManagerPortals.cpp
+++ src/vlGraphics/SceneManagerPortals.cpp
@@ -114,10 +114,7 @@ void SceneManagerPortals::renderPortal(Portal* portal)
     for(unsigned int i=0; i<portal->geometry().size(); ++i)
       vert_array->at(i) = portal->geometry()[i];
     portal_geom->drawCalls()->push_back( new vl::DrawArrays(vl::PT_LINE_LOOP, 0, (int)vert_array->size()) );
-    // mic fixme: this is weird!
 #if defined(VL_OPENGL)
-    portal_geom->drawCalls()->push_back( new vl::DrawArrays(vl::PT_LINE_LOOP, 0, (int)vert_array->size()) );
-    portal_geom->drawCalls()->push_back( new vl::DrawArrays(vl::PT_LINE_LOOP, 0, (int)vert_array->size()) );
     portal_geom->drawCalls()->push_back( new vl::DrawArrays(vl::PT_POLYGON,   0, (int)vert_array->size()) );
 #endif
     mPortalActorMap[portal] = new vl::Actor(portal_geom.get(), portal_fx.get(), NULL);
