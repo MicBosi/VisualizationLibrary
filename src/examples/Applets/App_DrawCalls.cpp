79baa366f33aadaa8fde157a25676a992f644d08
Michele <hello@michelebosi.com>
2011-07-27 00:37:49 +0000

    Finalized App_DrawCalls demo-test: checks primitive restart, base vertex, MultiDrawElements, index iterators, triangle iterators.

--- src/examples/Applets/App_DrawCalls.cpp
+++ src/examples/Applets/App_DrawCalls.cpp
@@ -29,8 +29,6 @@
 /*                                                                                    */
 /**************************************************************************************/
 
-// mic fixme: implement and cleaup this demo test
-
 #ifndef App_DrawCalls_INCLUDE_ONCE
 #define App_DrawCalls_INCLUDE_ONCE
 
@@ -41,8 +39,8 @@
 #include <vlGraphics/SceneManagerActorTree.hpp>
 #include <vlGraphics/Actor.hpp>
 #include <vlGraphics/Effect.hpp>
-#include <vlCore/Time.hpp>
 #include <vlGraphics/Light.hpp>
+#include <vlGraphics/TriangleStripGenerator.hpp>
 #include <vlGraphics/RenderingTree.hpp>
 
 using namespace vl;
@@ -85,33 +83,29 @@ class App_DrawCalls: public BaseDemo
 
     // merge indices
     ref<MultiDrawElementsUInt> mde = new MultiDrawElementsUInt(PT_QUADS);
-    ref<DrawElementsUInt> torus1_de = vl::cast<DrawElementsUInt>(torus1->drawCalls()->at(0));
-    ref<DrawElementsUInt> torus2_de = vl::cast<DrawElementsUInt>(torus2->drawCalls()->at(0));
-    ref<DrawElementsUInt> torus3_de = vl::cast<DrawElementsUInt>(torus3->drawCalls()->at(0));
+    ref<DrawCall> torus1_dc = torus1->drawCalls()->at(0);
+    ref<DrawCall> torus2_dc = torus2->drawCalls()->at(0);
+    ref<DrawCall> torus3_dc = torus3->drawCalls()->at(0);
 
-    mde->indices()->resize( torus1_de->indices()->size() + torus2_de->indices()->size() + torus3_de->indices()->size() );
-    memset(mde->indices()->ptr(), 0, mde->indices()->bytesUsed()); // mic fixme
+    size_t torus1_index_count = torus1_dc->countIndices();
+    size_t torus2_index_count = torus2_dc->countIndices();
+    size_t torus3_index_count = torus3_dc->countIndices();
+    mde->indices()->resize( torus1_index_count + torus2_index_count + torus3_index_count );
+
     MultiDrawElementsUInt::index_type* p_idx = mde->indices()->begin();
-    for( size_t i=0; i< torus1_de->indices()->size(); ++i, ++p_idx )
-    {
-      *p_idx = torus1_de->indices()->at(i);
-      VL_CHECK(*p_idx < 0xFFFF)
-    }
+    for( IndexIterator it = torus1_dc->indexIterator(); it.hasNext(); it.next(), ++p_idx )
+      *p_idx = it.index();
     
-    for( size_t i=0; i< torus2_de->indices()->size(); ++i, ++p_idx )
-    {
-      *p_idx = torus2_de->indices()->at(i) + torus1_vert->size();
-      VL_CHECK(*p_idx < 0xFFFF)
-    }
+    for( IndexIterator it = torus2_dc->indexIterator(); it.hasNext(); it.next(), ++p_idx )
+      *p_idx = it.index() + torus1_vert->size();
+    
+    for( IndexIterator it = torus3_dc->indexIterator(); it.hasNext(); it.next(), ++p_idx )
+      *p_idx = it.index() + torus1_vert->size() + torus2_vert->size();
 
-    for( size_t i=0; i< torus3_de->indices()->size(); ++i, ++p_idx )
-    {
-      *p_idx = torus3_de->indices()->at(i) + torus1_vert->size() + torus2_vert->size();
-      VL_CHECK(*p_idx < 0xFFFF)
-    }
-
+    VL_CHECK(p_idx == mde->indices()->end());
+    
     // define how many indices for each draw call
-    GLsizei count_vector[] = { torus1_de->indices()->size(), torus2_de->indices()->size(), torus3_de->indices()->size() };
+    GLsizei count_vector[] = { torus1_index_count, torus2_index_count, torus3_index_count };
     mde->setCountVector( count_vector, 3 );
 
     ref<Geometry> geom = new Geometry;
@@ -182,12 +176,138 @@ class App_DrawCalls: public BaseDemo
     sceneManager()->tree()->addActor( geom.get(), fx.get(), NULL );
   }
 
+  void setupScene_PrimitiveRestart()
+  {
+    // QUICK TEST RESULTS:
+    // - PT_TRIANGLE_STRIP merged: ~250 FPS (1 draw call).
+    // - PT_TRIANGLE_STRIP merged with primitive restart: ~250 FPS (1 draw call).
+    // - PT_TRIANGLE_STRIP non-merged: ~50 FPS (169 draw calls in total).
+    // - PT_QUADS: ~250 FPS (1 draw call).
+    // - PT_TRIANGLE_STRIP merged with MultiDrawElements: ~175 FPS.
+    // ERGO:
+    // - PT_TRIANGLE_STRIP are not faster than PT_QUADS.
+    // - If using PT_TRIANGLE_STRIP just merge them into one, no need for primitive restart.
+    // - Primitive restart is mainly useful for PT_POLYGON and PT_TRIANGLE_FAN where merging is not possible.
+    // - If you use PT_TRIANGLES or PT_QUADS you don't need primitive restart at all of course.
+
+    ref<Geometry> teapot_p_restart = makeTeapot( vec3(0,0,0), 10, 8 );
+    teapot_p_restart->computeNormals();
+    TriangleStripGenerator::stripfy(teapot_p_restart.get(), 22, false, false, true);
+    ref<Geometry> teapot_multi = teapot_p_restart->shallowCopy();
+    
+    // teapot_p_restart will merge all triangle strip using primitive restart
+    teapot_p_restart->mergeDrawCallsWithPrimitiveRestart(PT_TRIANGLE_STRIP);
+    
+    // teapot_multi will merge all triangle strip using MultiDrawElements
+    teapot_multi->mergeDrawCallsWithMultiDrawElements(PT_TRIANGLE_STRIP);
+
+    ref<Effect> red_fx = new Effect;
+    red_fx->shader()->setRenderState( new Light(0) );
+    red_fx->shader()->enable(vl::EN_LIGHTING);
+    red_fx->shader()->enable(vl::EN_DEPTH_TEST);
+    red_fx->shader()->gocMaterial()->setDiffuse( vl::crimson );
+
+    ref<Effect> green_fx = new Effect;
+    green_fx->shader()->setRenderState( new Light(0) );
+    green_fx->shader()->enable(vl::EN_LIGHTING);
+    green_fx->shader()->enable(vl::EN_DEPTH_TEST);
+    green_fx->shader()->gocMaterial()->setDiffuse( vl::green );
+
+    Actor* pot_actor1 = sceneManager()->tree()->addActor( teapot_p_restart.get(), red_fx.get(), new Transform(mat4::getTranslation(-10,0,0)) );
+    Actor* pot_actor2 = sceneManager()->tree()->addActor( teapot_multi.get(), green_fx.get(), new Transform(mat4::getTranslation(+10,0,0)) );
+    pot_actor1->transform()->computeWorldMatrix();
+    pot_actor2->transform()->computeWorldMatrix();
+
+    // Test Scene
+    //for(int x=-50; x<=50; x+=5)
+    //for(int y=-50; y<=50; y+=5)
+    //  sceneManager()->tree()->addActor( teapot.get(), fx.get(), new Transform( mat4::getTranslation(x,y,0) ) )->transform()->computeWorldMatrix();
+  }
+
+  bool testTriangleIterators()
+  {
+    ref<Geometry> teapot1 = makeTeapot( vec3(0,0,0), 10, 8);
+    teapot1->computeNormals();
+    TriangleStripGenerator::stripfy(teapot1.get(), 22, false, false, true);
+
+    // remove the last draw calls generated by stripfy() which is a PT_TRIANGLES
+    teapot1->drawCalls()->eraseAt( teapot1->drawCalls()->size() -1 );
+
+    ref<Geometry> teapot2 = teapot1->shallowCopy();
+    ref<Geometry> teapot3 = teapot1->shallowCopy();
+
+    teapot2->mergeDrawCallsWithPrimitiveRestart(PT_TRIANGLE_STRIP);
+    teapot3->mergeDrawCallsWithMultiDrawElements(PT_TRIANGLE_STRIP);
+
+    std::vector<int> triangles1;
+    std::vector<int> triangles2;
+    std::vector<int> triangles3;
+
+    int index_count1 = 0;
+    int index_count2 = 0;
+    int index_count3 = 0;
+
+    // go backwards to reproduce the triangle order generated by mergeDrawCalls*()
+    for(int i=teapot1->drawCalls()->size(); i--; )
+    {
+      index_count1 += teapot1->drawCalls()->at(i)->countIndices();
+      for( TriangleIterator it=teapot1->drawCalls()->at(i)->triangleIterator(); it.hasNext(); it.next() )
+      {
+        triangles1.push_back( it.a() );
+        triangles1.push_back( it.b() );
+        triangles1.push_back( it.c() );
+      }
+    }
+    index_count2 = teapot2->drawCalls()->at(0)->countIndices();
+    index_count3 = teapot3->drawCalls()->at(0)->countIndices();
+    VL_CHECK( index_count1 == index_count2 );
+    VL_CHECK( index_count1 == index_count3 );
+
+    for( TriangleIterator it=teapot2->drawCalls()->at(0)->triangleIterator(); it.hasNext(); it.next() )
+    {
+      triangles2.push_back( it.a() );
+      triangles2.push_back( it.b() );
+      triangles2.push_back( it.c() );
+    }
+
+    for( TriangleIterator it=teapot3->drawCalls()->at(0)->triangleIterator(); it.hasNext(); it.next() )
+    {
+      triangles3.push_back( it.a() );
+      triangles3.push_back( it.b() );
+      triangles3.push_back( it.c() );
+    }
+    VL_CHECK( triangles1.size() == triangles2.size() );
+    VL_CHECK( triangles1.size() == triangles3.size() );
+
+    bool ok1 = memcmp( &triangles1[0], &triangles2[0], sizeof(triangles1[0])*triangles1.size() ) == 0;
+    bool ok2 = memcmp( &triangles1[0], &triangles3[0], sizeof(triangles1[0])*triangles1.size() ) == 0;
+
+    return ok1 && ok2 && triangles1.size() == triangles2.size() && triangles1.size() == triangles3.size() && index_count1 == index_count2 && index_count1 == index_count3;
+  }
+
   void initEvent()
   {
     vl::Log::info(appletInfo());
 
-    setupScene_BaseVertex();
-    setupScene_MultiDrawElements();
+    if(!testTriangleIterators())
+    {
+      Log::bug("App_DrawCalls::testTriangleIterators() failed!\n");
+      VL_TRAP();
+    }
+    else
+    {
+      if (Has_Base_Vertex)
+        setupScene_BaseVertex();
+      else
+        Log::error("Base-vertex functionality not supported, test skipped.\n");
+
+      setupScene_MultiDrawElements();
+
+      if (Has_Primitive_Restart)
+        setupScene_PrimitiveRestart();
+      else
+        Log::error("Primitive-restart functionality not supported, test skipped.\n");
+    }
   }
 
 };
