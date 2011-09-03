1a76a1f8347acdedcef135b2e758e07ec5a26794
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2011-09-03 18:53:28 +0000

    Geometry: implemented completely general shrinkDrawCalls() and triangulateDrawCalls(). Updated makeGLESFriendly() method.

--- trunk/src/vlGraphics/MultiDrawElements.hpp
+++ trunk/src/vlGraphics/MultiDrawElements.hpp
@@ -55,7 +55,7 @@ namespace vl
 
   public:
     /** Returns whether the primitive-restart functionality is enabled or not. See http://www.opengl.org/registry/specs/NV/primitive_restart.txt */
-    bool primitiveRestartEnabled() const { return mPrimitiveRestartEnabled; }
+    virtual bool primitiveRestartEnabled() const { return mPrimitiveRestartEnabled; }
     
     /** Enables the primitive-restart functionality. See http://www.opengl.org/registry/specs/NV/primitive_restart.txt */
     void setPrimitiveRestartEnabled(bool enabled) { mPrimitiveRestartEnabled = enabled; }
@@ -152,6 +152,7 @@ namespace vl
     typedef typename arr_type::scalar_type index_type;
     //! The special index which identifies a primitive restart. By default it is set to ~0 that is 0xFF, 0xFFFF, 0xFFFFFFFF respectively for GLubyte, GLushort, GLuint index types. */
     static const index_type primitive_restart_index = index_type(~0);
+    virtual unsigned int primitiveRestartIndex() { return (unsigned int)primitive_restart_index; }
 
   public:
     MultiDrawElements(EPrimitiveType primitive = PT_TRIANGLES)
