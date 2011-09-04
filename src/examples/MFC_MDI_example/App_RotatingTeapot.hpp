7af32b114eb4be81472a12d6ba02e02c1c5b280b
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2011-09-04 01:19:10 +0000

    fixed MFC example compilation

--- src/examples/MFC_MDI_example/App_RotatingTeapot.hpp
+++ src/examples/MFC_MDI_example/App_RotatingTeapot.hpp
@@ -61,7 +61,7 @@ public:
     // enable depth test and lighting 
     effect->shader()->enable(vl::EN_DEPTH_TEST);
     // add a Light to the scene, since no Transform is associated to the Light it will follow the camera 
-    effect->shader()->setRenderState( new vl::Light(0) );
+    effect->shader()->setRenderState( new vl::Light, 0 );
     // enable the standard OpenGL lighting 
     effect->shader()->enable(vl::EN_LIGHTING);
     // set the front and back material color of the cube 
@@ -79,7 +79,7 @@ public:
   virtual void updateScene()
   {
     // rotates the cube around the Y axis 45 degrees per second 
-    vl::Real degrees = vl::Time::currentTime() * 45.0f;
+    vl::real degrees = vl::Time::currentTime() * 45.0f;
     vl::mat4 matrix = vl::mat4::getRotation( degrees, 0,1,0 );
     mCubeTransform->setLocalMatrix( matrix );
   }
