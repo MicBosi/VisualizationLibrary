9ce92db96c24f70d621a1f9ed663606d511a8868
michele.bosi <michele.bosi@e3458a3e-9034-11de-b664-3b115b7b7a9b>
2011-09-12 16:00:45 +0000

    update GLSL shader, fixed toyball shader

--- trunk/data/glsl/examples/stripes.fs
+++ trunk/data/glsl/examples/stripes.fs
@@ -1,3 +1,18 @@
+/**************************************************************************************/
+/*                                                                                    */
+/*  Copyright (c) 2005-2011, Michele Bosi.                                            */
+/*  All rights reserved.                                                              */
+/*                                                                                    */
+/*  This file is part of Visualization Library                                        */
+/*  http://www.visualizationlibrary.org                                               */
+/*                                                                                    */
+/*  Released under the OSI approved Simplified BSD License                            */
+/*  http://www.opensource.org/licenses/bsd-license.php                                */
+/*                                                                                    */
+/**************************************************************************************/
+
+// adaptation from the "Orange Book" (http://www.3dshaders.com)
+
 // requires "stripes.vs"
 
 varying float V;
