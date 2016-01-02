d06aeb7277e6e1c22255e9fa13ca191600d183da
Michele <hello@michelebosi.com>
2016-01-02 23:05:25 +0000

    update

--- src/vlEGL/EGLWindow.hpp
+++ src/vlEGL/EGLWindow.hpp
@@ -1,7 +1,7 @@
 /**************************************************************************************/
 /*                                                                                    */
 /*  Visualization Library                                                             */
-/*  http://www.visualizationlibrary.org                                               */
+/*  http://visualizationlibrary.org                                                   */
 /*                                                                                    */
 /*  Copyright (c) 2005-2011, Michele Bosi                                             */
 /*  All rights reserved.                                                              */
