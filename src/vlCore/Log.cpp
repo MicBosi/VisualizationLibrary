122698e3b77eae8411a1e30e3d1c2130dd488280
Michele <hello@michelebosi.com>
2016-01-03 02:35:08 +0000

    VL_Build renamed VL_Patch and support string patch to allow beta and alpha suffixes

--- src/vlCore/Log.cpp
+++ src/vlCore/Log.cpp
@@ -199,7 +199,7 @@ void Log::logSystemInfo()
   #endif
 
   print( Say("Visualization Library v%n.%n.%n [%s]\n%s - %s - %s compiler [%s] [%s]\n") 
-    << VL_Major << VL_Minor << VL_Build 
+    << VL_Major << VL_Minor << VL_Patch 
     << (sizeof(vec3) == sizeof(fvec3) ? "f32" : "f64")
     << __DATE__ << __TIME__ << compiler << build_type 
     << (sizeof(void*) == 4 ? "x32" : "x64") );
