cec70f5b9640222403b5825a564e89619907e758
Michele <hello@michelebosi.com>
2016-01-03 01:10:15 +0000

    updated version extraction regexs

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -11,9 +11,9 @@ fi
 # Default location is . but it can be overridden externally
 : ${VL_DOCS_OUTPUT_DIRECTORY:=.}
 export VL_DOCS_OUTPUT_DIRECTORY
-export VL_VERSION_MAJOR=`grep VL_VERSION_MAJOR ../CMakeLists.txt  | head -n 1 | grep -oh '[0-9]*'`
-export VL_VERSION_MINOR=`grep VL_VERSION_MINOR ../CMakeLists.txt  | head -n 1 | grep -oh '[0-9]*'`
-export VL_VERSION_PATCH=`grep VL_VERSION_PATCH ../CMakeLists.txt  | head -n 1 | grep -oh '[0-9]*'`
+export VL_VERSION_MAJOR=`grep VL_VERSION_MAJOR ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9]+'`
+export VL_VERSION_MINOR=`grep VL_VERSION_MINOR ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9]+'`
+export VL_VERSION_PATCH=`grep VL_VERSION_PATCH ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9][-0-9a-z]+'`
 export VL_VERSION="v$VL_VERSION_MAJOR.$VL_VERSION_MINOR.$VL_VERSION_PATCH"
 rm -rf html-out/
 rm -rf $VL_DOCS_OUTPUT_DIRECTORY/html-out/
