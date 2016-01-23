68f7f7437b3630fed37a1b249a8f61f21d179438
Michele <hello@michelebosi.com>
2016-01-23 21:07:45 +0000

    fixed gendocs.sh

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -13,7 +13,7 @@ fi
 export VL_DOCS_OUTPUT_DIRECTORY
 export VL_VERSION_MAJOR=`grep VL_VERSION_MAJOR ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9]+'`
 export VL_VERSION_MINOR=`grep VL_VERSION_MINOR ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9]+'`
-export VL_VERSION_PATCH=`grep VL_VERSION_PATCH ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9][-0-9a-z]+'`
+export VL_VERSION_PATCH=`grep VL_VERSION_PATCH ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9]+[-0-9a-z]*'`
 export VL_VERSION="v$VL_VERSION_MAJOR.$VL_VERSION_MINOR.$VL_VERSION_PATCH"
 rm -rf html-out/
 rm -rf $VL_DOCS_OUTPUT_DIRECTORY/html-out/
