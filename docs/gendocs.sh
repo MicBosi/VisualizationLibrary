ccc8e3a0480a2474c786263c05a0a758292bfd7e
Michele Bosi <hello@michelebosi.com>
2016-04-13 11:37:31 +0100

    update

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -2,7 +2,9 @@
 set -o nounset # error if unset variables
 set -o errexit # exit if error
 
-DOXYVER=`doxygen --version`
+: ${PATH_TO_DOXYGEN:='/c/Doxygen-1.7.2/doxygen.exe'}
+
+DOXYVER=`${PATH_TO_DOXYGEN} --version`
 if [ "$DOXYVER" != "1.7.2" ]; then
     echo 'Doxygen version 1.7.2 required but found version' `doxygen --version`
     exit 1
@@ -15,7 +17,8 @@ export VL_VERSION_MAJOR=`grep VL_VERSION_MAJOR ../CMakeLists.txt  | head -n 1 | 
 export VL_VERSION_MINOR=`grep VL_VERSION_MINOR ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9]+'`
 export VL_VERSION_PATCH=`grep VL_VERSION_PATCH ../CMakeLists.txt  | head -n 1 | grep -Poh '[0-9]+[-0-9a-z]*'`
 export VL_VERSION="v$VL_VERSION_MAJOR.$VL_VERSION_MINOR.$VL_VERSION_PATCH"
+echo VL_VERSION=${VL_VERSION}
 rm -rf html-out/
 rm -rf $VL_DOCS_OUTPUT_DIRECTORY/html-out/
-doxygen
+${PATH_TO_DOXYGEN}
 cp -a html-in/. $VL_DOCS_OUTPUT_DIRECTORY/html-out/
